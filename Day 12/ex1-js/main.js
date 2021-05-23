1de74f03dacc09ceb7b7a300529e88890553c6aa
EliyaMs <eliya205@gmail.com>
2021-05-23 16:56:15 +0300

    ex1 v1.2

--- Day 12/ex1-js/main.js
+++ Day 12/ex1-js/main.js
@@ -16,10 +16,12 @@ Calculator.prototype.mult = function () {
     return this.a * this.b;
 }
 
-Calculator.prototype.log = function () {
-    console.log(this.a + ' + ' + this.b + ' = ' + this.add());
-    console.log(this.a + ' - ' + this.b + ' = ' + this.sub());
-    console.log(this.a + ' * ' + this.b + ' = ' + this.mult());
+Calculator.prototype.log = function () { // print all the functions
+    console.log(
+        this.a + ' + ' + this.b + ' = ' + this.add() + '\n' + 
+        this.a + ' - ' + this.b + ' = ' + this.sub() + '\n' + 
+        this.a + ' * ' + this.b + ' = ' + this.mult()
+    );
 }
 
 var c1 = new Calculator(5, 10);
@@ -29,6 +31,8 @@ var c2 = new Calculator(6, 10);
 c2.log();
 
 
-setTimeout(() => {
+setTimeout(_ => {
     c1.log();
-}, 2500);
+}, 2000);
+//<Equals>
+setTimeout(c2.log.bind(c2), 3000);
