037cc1c81c2d41dbb68e03d444af95d2ecdb678c
EliyaMs <eliya205@gmail.com>
2021-05-24 18:47:41 +0300

    ex2-js v1

---
+++ Day 13/bin/exam.js
@@ -0,0 +1,35 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Exam = void 0;
+var Exam = /** @class */ (function () {
+    function Exam() {
+        this.questions = [];
+    }
+    Exam.prototype.addQuestion = function (question) {
+        if (question != null)
+            this.questions.push(question);
+    };
+    Exam.prototype.print = function () {
+        for (var _i = 0, _a = this.questions; _i < _a.length; _i++) {
+            var question = _a[_i];
+            console.log(question.caption);
+            for (var _b = 0, _c = question.answers; _b < _c.length; _b++) {
+                var answer = _c[_b];
+                console.log(answer);
+            }
+        }
+    };
+    Exam.prototype.grade = function (answers) {
+        var Corrects = 0;
+        var minLength = answers.length > this.questions.length ? answers.length : this.questions.length;
+        for (var i = 0; i < minLength; i++) {
+            if (typeof (this.questions[i]) !== 'undefined')
+                if (answers[i] === this.questions[i].correctIndex)
+                    Corrects++;
+        }
+        return (Corrects / answers.length) * 100;
+    };
+    return Exam;
+}());
+exports.Exam = Exam;
+//# sourceMappingURL=exam.js.map
